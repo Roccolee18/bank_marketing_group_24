53956e12af7ffcecb6fce0ea325ae57d0b32fa3e
Rocco Lee <roccolee18@gmail.com>
2025-12-12 11:11:58 -0800

    Added 2/3 test scripts

--- test/test_create_feat_imp.py
+++ test/test_create_feat_imp.py
@@ -1,10 +1,19 @@
 import pytest
 from pathlib import Path
 import os
+from PIL import Image
 
 def test_create_feat_imp():
-    # Using PyTest assert statements to make sure figures generated from the fit_and_predict script exist
+    # Testing if genereated figure exists
     project_root = Path(__file__).parent.parent
     feat_importance_directory = project_root / "results" / "figures" / "feature_importance.jpg"
 
     assert feat_importance_directory.exists(), f"File {feat_importance_directory} does not exist"
+
+    # Testing dimensions of genereated figure
+    with Image.open(feat_importance_directory) as img:
+        width, height = img.size
+        assert width == 2480
+        assert height == 1756
+
+test_create_feat_imp()
